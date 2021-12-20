521ef8f240f041af23cb45714bad1a954dbecf33
Purich Purisinsits <purich.puri@gmail.com>
2021-12-20 19:50:20 +1100

    update api code

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="nav">
+    <img src="@/assets/pokeball-img.png" alt="pokemon logo" class="w-10 h-10" />
+    <div class="flex gap-2">
+      <router-link to="/">HOME</router-link> |
+      <router-link to="/about">ABOUT</router-link>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3 class="flex justify-center text-4xl">Pokedex</h3>
+    <div v-for="pkmn in pokemon" :key="pkmn">
+      {{ pkmn.name }}
+    </div>
   </div>
 </template>
 
 <script>
+// import { reactive } from "@vue/reactivity";
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+  // setup(){
+  //   const state = reactive({
+  //     pokemon:[],
+  //     urlIdLookup: {}
+  //   })
+  // },
+
+  data() {
+    return {
+      pokemon: [],
+    };
+  },
+  methods: {
+    async getPokemon() {
+      // fetch("https://pokeapi.co/api/v2/pokemon/")
+      // .then((res) => res.json)
+      // .then((data) => {
+      //   console.log()})
+      const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151/");
+      const { results } = await res.json();
+      console.log(results);
+      this.pokemon = results;
+    },
+  },
+  beforeMount() {
+    this.getPokemon();
+  },
+};
 </script>
+
+<style scoped>
+.home {
+  height: 100%;
+  background-color: antiquewhite;
+}
+</style>
